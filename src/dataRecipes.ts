28accb9957876968532d295feba2dda736719461
XicoCorreia <xicocorreia17@gmail.com>
2024-11-12 18:21:10 +0100

    Category and recipes zoom fix

--- src/dataRecipes.ts
+++ src/dataRecipes.ts
@@ -93,7 +93,104 @@ export const sample_recipes: Recipe[] = [
             {title: "Fat", description: "NDF"},
             {title: "Fiber", description: "NDF"}
         ]
+    },
+    {
+        name: 'Apple Turnovers',
+        id: '5',
+        imageUrl: '/assets/images/recipes/apple_turnovers.jpg',
+        date: "05 May 2024",
+        categories: [EnumCategories.Dessert, EnumCategories.AirFryer],
+        introduction: {
+            introduction: "I always loved apples, and as everybody knows, apple and cinnamon belong together. So, I decided to combine these timeless "+
+            "flavors into a delightful pastry that captures the essence of autumn in every bite.",
+            description: "Indulge in the comforting embrace of our apple turnover, where buttery layers of pastry envelop a generous filling of cinnamon-kissed "+
+            "apples. Each bite offers a symphony of flavors – the tartness of the apples harmonizing with the warmth of cinnamon, all encased in a crisp, golden "+
+            "crust. Baked to perfection, our turnovers boast a tantalizing aroma that beckons with promises of sweet satisfaction. Whether enjoyed as a morning "+
+            "treat or an afternoon delight, our apple turnovers are sure to evoke nostalgic memories and leave taste buds craving more. Experience the epitome of "+
+            "autumnal bliss with every delectable bite."
+        },
+        ingredients: {
+            introduction: "Here is everything you will need:",
+            content: [
+                { title: "Apples", description: "Our main character in the pastry." },
+                { title: "Cinnamon and sugar/stevia", description: "I used stevia to make it less caloric, but you can use whatever you have at home." },
+                { title: "Store-bought puff pastry", description: "I bought mine at Lidl but choose your favorite brand." },
+                { title: "Lemon", description: "The secret ingredient to make the sauce juicy and not too sweet." },
+                { title: "Milk", description: "Used to give that golden and crispy color to the pastry. You can use either milk or eggs." }
+            ],
+            image: '/assets/images/recipes/apple_turnovers.jpg',
+            subtitle: 'What you will need to make Apple Turnovers'
+        },
+        steps: {
+            introduction: "As you can see below, it is easy, fast, and delicious:",
+            content: [
+                { description: "Cut half of an apple into cubes." },
+                { description: "Add both cinnamon and sugar in a pot, mix them together, and add the apple to coat them." },
+                { description: "Add some lemon juice to the apples." },
+                { description: "Cut the pastry into a rectangle and put the coated apples in the middle." },
+                { description: "Close the pastry by bringing the corners to the middle and twisting. Brush milk onto the puff pastry." },
+                { description: "Place it in the air fryer at 180ºC for 10 minutes and enjoy!" }
+            ],
+            image: '/assets/images/recipes/steps.jpg',
+            subtitle: 'Recipe Apple Turnovers'
+        },
+        nutrituional_values: [
+            {title: "Calories", description: "NDF"},
+            {title: "Carbohydrates", description: "NDF"},
+            {title: "Protein", description: "NDF"},
+            {title: "Fat", description: "NDF"},
+            {title: "Fiber", description: "NDF"}
+        ]
+    },
+    {
+        name: 'Apple Turnovers',
+        id: '5',
+        imageUrl: '/assets/images/recipes/apple_turnovers.jpg',
+        date: "05 May 2024",
+        categories: [EnumCategories.Dessert, EnumCategories.AirFryer],
+        introduction: {
+            introduction: "I always loved apples, and as everybody knows, apple and cinnamon belong together. So, I decided to combine these timeless "+
+            "flavors into a delightful pastry that captures the essence of autumn in every bite.",
+            description: "Indulge in the comforting embrace of our apple turnover, where buttery layers of pastry envelop a generous filling of cinnamon-kissed "+
+            "apples. Each bite offers a symphony of flavors – the tartness of the apples harmonizing with the warmth of cinnamon, all encased in a crisp, golden "+
+            "crust. Baked to perfection, our turnovers boast a tantalizing aroma that beckons with promises of sweet satisfaction. Whether enjoyed as a morning "+
+            "treat or an afternoon delight, our apple turnovers are sure to evoke nostalgic memories and leave taste buds craving more. Experience the epitome of "+
+            "autumnal bliss with every delectable bite."
+        },
+        ingredients: {
+            introduction: "Here is everything you will need:",
+            content: [
+                { title: "Apples", description: "Our main character in the pastry." },
+                { title: "Cinnamon and sugar/stevia", description: "I used stevia to make it less caloric, but you can use whatever you have at home." },
+                { title: "Store-bought puff pastry", description: "I bought mine at Lidl but choose your favorite brand." },
+                { title: "Lemon", description: "The secret ingredient to make the sauce juicy and not too sweet." },
+                { title: "Milk", description: "Used to give that golden and crispy color to the pastry. You can use either milk or eggs." }
+            ],
+            image: '/assets/images/recipes/apple_turnovers.jpg',
+            subtitle: 'What you will need to make Apple Turnovers'
+        },
+        steps: {
+            introduction: "As you can see below, it is easy, fast, and delicious:",
+            content: [
+                { description: "Cut half of an apple into cubes." },
+                { description: "Add both cinnamon and sugar in a pot, mix them together, and add the apple to coat them." },
+                { description: "Add some lemon juice to the apples." },
+                { description: "Cut the pastry into a rectangle and put the coated apples in the middle." },
+                { description: "Close the pastry by bringing the corners to the middle and twisting. Brush milk onto the puff pastry." },
+                { description: "Place it in the air fryer at 180ºC for 10 minutes and enjoy!" }
+            ],
+            image: '/assets/images/recipes/steps.jpg',
+            subtitle: 'Recipe Apple Turnovers'
+        },
+        nutrituional_values: [
+            {title: "Calories", description: "NDF"},
+            {title: "Carbohydrates", description: "NDF"},
+            {title: "Protein", description: "NDF"},
+            {title: "Fat", description: "NDF"},
+            {title: "Fiber", description: "NDF"}
+        ]
     }
+    
 ];
 
 
