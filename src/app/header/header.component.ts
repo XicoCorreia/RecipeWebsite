c82802cc1e187d632ade77593189f439217b7a0f
XicoCorreia <xicocorreia17@gmail.com>
2024-11-13 20:02:43 +0100

    fix header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,14 +8,23 @@ import { FoodService } from '../services/food.service';
 })
 export class HeaderComponent {
 
-  icon:String = '';
+  icon: string = '';
+  isDropdownOpen: boolean = false;  // Flag to track dropdown state
 
-  constructor(private foodService:FoodService) {
-
-  }
+  constructor(private foodService: FoodService) {}
 
   ngOnInit(): void {
     this.icon = this.foodService.getIcon();
   }
 
+  // Toggle the dropdown visibility on click
+  toggleDropdown(event: Event): void {
+    event.preventDefault();  // Prevent default behavior of the link
+    this.isDropdownOpen = !this.isDropdownOpen;  // Toggle the dropdown visibility
+  }
+
+  // Close the dropdown when an option is selected
+  closeDropdown(): void {
+    this.isDropdownOpen = false;  // Close the dropdown
+  }
 }
