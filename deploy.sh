044b56364c8f5a82f2a143354406eda346004f79
XicoCorreia <xicocorreia17@gmail.com>
2024-11-16 20:02:27 +0100

    cat page updated

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+echo "Deleting previous dist"
+rm -rf dist
+
 # Run prerender command for the recipe-website project
 echo "Running prerender..."
 ng run recipe-website:prerender
+
+# Create CNAME file inside dist/recipe-website/browser directory with "nelasrecipes.com"
+echo "Creating CNAME file..."
+echo "nelasrecipes.com" > dist/recipe-website/browser/CNAME
 
 # Deploy the build to GitHub Pages
 echo "Deploying to GitHub Pages..."
