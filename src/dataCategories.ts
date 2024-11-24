eee2f4843c56a1bf36bf5535b85efcbd00220abe
XicoCorreia <xicocorreia17@gmail.com>
2024-11-24 14:19:27 +0100

    New photos

--- src/dataCategories.ts
+++ src/dataCategories.ts
@@ -16,7 +16,7 @@ export const course_categories: Category[] = [
     {id: '2', 
     name: EnumCategories.Breakfast, 
     path:"breakfast-recipes",
-    imageUrl:'assets/images/categories/breakfast.jpeg',
+    imageUrl:'assets/images/categories/breakfast.jpg',
     description: "Start your day off right with our breakfast delights! Dive into a world of wholesome goodness and energizing flavors with "+
     "our diverse selection of breakfast recipes. Whether you're a fan of hearty oats, refreshing smoothies, satisfying sandwiches, or crave "+
     "something more, our breakfast category has something to suit every palate. Join me in celebrating the most important meal of the day with "+
@@ -25,7 +25,7 @@ export const course_categories: Category[] = [
     {id: '3', 
     name: EnumCategories.Dessert, 
     path:"dessert-recipes",
-    imageUrl:'assets/images/categories/dessert.jpeg',
+    imageUrl:'assets/images/categories/dessert.jpg',
     description: "Satisfy your sweet tooth and indulge in decadent delights with our irresistible dessert recipes! Dive into a world of "+
      "confectionery bliss where every bite is a celebration of flavor and creativity. From classic cakes and cookies to exotic treats from "+
      "around the globe, our desserts category offers a tantalizing array of options to suit every craving.Whether you're hosting a special "+
@@ -34,7 +34,7 @@ export const course_categories: Category[] = [
     {id: '4', 
     name: EnumCategories.SidesAppetizers, 
     path:"sides-recipes",
-    imageUrl:'assets/images/categories/sides.jpeg',
+    imageUrl:'assets/images/categories/appetizers.jpg',
     description: "Enhance every dining experience with our tantalizing selection of sides and appetizers! From crisp, garden-fresh salads to crispy, "+
     "golden fries and beyond, our menu of accompaniments elevates every dish. Delve into a world of flavors and textures with our diverse range of "+
     "appetizers, designed to tantalize your taste buds and elevate your meal. Whether you seek light and nutritious options to balance your plate or "+
@@ -46,7 +46,7 @@ export const categories_ingredient: Category[] = [
     {id: '11', 
     name: EnumCategories.Beef,
     path:"main-recipes/beef", 
-    imageUrl:'assets/images/categories/beef.jpeg',
+    imageUrl:'assets/images/categories/beef.jpg',
     description: "Savor the richness of our beef recipes! From tender steaks to hearty stews and beyond, our collection showcases "+
     "the versatility and deliciousness of beef. Explore a world of savory flavors and satisfying textures as you discover new favorites "+
     "and timeless classics. Whether you're craving a juicy burger, a comforting pot roast, or something in between, our beef recipes are "+
@@ -56,7 +56,7 @@ export const categories_ingredient: Category[] = [
     {id: '12', 
     name: EnumCategories.Chicken, 
     path:"main-recipes/chicken",
-    imageUrl:'assets/images/categories/chicken.jpeg',
+    imageUrl:'assets/images/categories/chicken.jpg',
     description: "Feast on a world of flavor with our chicken recipes! From succulent grilled breasts to savory stews and more, our "+
     "collection of chicken dishes offers something for every palate. Explore a variety of cooking methods and flavor profiles as you "+
     "discover new ways to enjoy this versatile protein. Whether you're in the mood for comforting classics or bold, adventurous flavors, "+
@@ -96,7 +96,7 @@ export const categories_ingredient: Category[] = [
     {id: '16', 
     name: EnumCategories.Oats,
     path:"breakfast-recipes/oats",
-    imageUrl:'assets/images/categories/oats.png',
+    imageUrl:'assets/images/categories/oats.jpg',
     description: "Discover the versatility of oats in our collection of oat-based recipes. From comforting porridges to innovative oat bars, "+
     "our selection showcases the hearty and healthful qualities of oats. Ideal for breakfasts, snacks, or even savory dishes, these recipes "+
     "provide delicious options to fuel your day with wholesome goodness.",
@@ -108,7 +108,7 @@ export const categories_special: Category[] = [
     {id: '31', 
     name: EnumCategories.LowCalorie,
     path:"lowcalorie-recipes",
-    imageUrl:'assets/images/categories/lowcalorie.jpeg',
+    imageUrl:'assets/images/categories/lowcalorie.jpg',
     description: "Embark on a journey of culinary delight with my low-calorie recipes, crafted with a passion for flavor and a dedication to "+
     "wellness. As someone who found joy in cooking through a focus on healthier eating, I understand the importance of meals that are both "+
     "satisfying and nourishing. My low-calorie category offers a treasure trove of dishes that prioritize wholesome ingredients without "+
@@ -195,7 +195,7 @@ export const sub_categories: Category[] = [
     {id: '51', 
     name:EnumCategories.Burritos,
     path:"main-recipes/burritos", 
-    imageUrl:'assets/images/categories/burrito.jpeg',
+    imageUrl:'assets/images/categories/burrito.jpg',
     description: 
     "Discover the delightful world of burritos! From classic Mexican-style burritos bursting with seasoned meats, "+ 
     "beans, and cheese, to inventive fusion creations featuring bold flavors from around the globe, my burrito " +
@@ -206,7 +206,7 @@ export const sub_categories: Category[] = [
     {id: '52', 
     name:EnumCategories.Smoothies,
     path:"breakfast-recipes/smoothies",
-    imageUrl:'assets/images/categories/smoothie.png',
+    imageUrl:'assets/images/categories/smoothie.jpg',
     description: "Energize your day with our delicious smoothie recipes. Packed with fruits, vegetables, and other nutritious ingredients, "+
     "these smoothies are perfect for a quick breakfast or a revitalizing snack. Explore a spectrum of flavors and combinations that are not "+
     "only tasty but also health-boosting, designed to keep you hydrated and satisfied.",
@@ -215,7 +215,7 @@ export const sub_categories: Category[] = [
     {id: '53', 
     name:EnumCategories.Sandwhiches,
     path:"breakfast-recipes/sandwhiches",
-    imageUrl:'assets/images/categories/sandwhich.jpg',
+    imageUrl:'assets/images/categories/sandwich.jpg',
     description: "Kickstart your morning with our collection of hearty breakfast sandwiches. Each recipe is packed with flavorful ingredients "+
     "like eggs, cheese, and meats, all nestled between slices of freshly toasted bread or bagels. Whether you're craving something classic or "+
     "looking for a new twist on your morning routine, our sandwiches are sure to energize your day with deliciousness.",
@@ -224,7 +224,7 @@ export const sub_categories: Category[] = [
     {id: '54', 
     name: EnumCategories.Cake,
     path:"dessert-recipes/cake",
-    imageUrl:'assets/images/categories/cake.png',
+    imageUrl:'assets/images/categories/cake.jpg',
     description: "Indulge in our wide array of cake recipes, perfect for any celebration or sweet craving. From classic chocolate to sophisticated "+
     "fruit layers, each recipe promises moist, fluffy textures and rich flavors. Whether you're a novice baker or a seasoned pastry chef, find your "+
     "next favorite cake here and make every occasion a little sweeter.",
