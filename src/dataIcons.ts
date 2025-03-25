8248e135f603776faf601e4a6b11e53cc9ee1160
XicoCorreia <xicocorreia17@gmail.com>
2025-03-25 20:07:49 +0100

    test

--- src/dataIcons.ts
+++ src/dataIcons.ts
@@ -4,7 +4,7 @@ export const sample_icons: Icon[] = [
     {id: '1', 
     name:"Instagram", 
     imageUrl:'assets/images/icons/instagram.ico',
-    router: 'https://www.instagram.com/xicocorreia17/'
+    router: 'https://www.instagram.com/xicocorreia17'
     },
     {id: '2', 
     name:"Twitter", 
@@ -14,12 +14,12 @@ export const sample_icons: Icon[] = [
     {id: '3', 
     name:"Facebook", 
     imageUrl:'assets/images/icons/facebook.ico',
-    router: 'https://www.facebook.com/francisco.correia.39/'
+    router: 'https://www.facebook.com/francisco.correia.39'
     },
     {id: '4', 
     name:"LinkedIn", 
     imageUrl:'assets/images/icons/linkedin.ico',
-    router: 'https://www.linkedin.com/in/francisco-correia7/'
+    router: 'https://www.linkedin.com/in/francisco-correia7'
     },
     {id: '5', 
     name:"Github", 
